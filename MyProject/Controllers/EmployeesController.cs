94962bada749f237a173db149dd8ab12bc9c9f24
ArunKumar <arunkumar.e@outlook.com>
2023-03-01 10:43:06 +0530

    Add project files.

---
+++ MyProject/Controllers/EmployeesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyProject.Models;
+
+namespace MyProject.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        public IActionResult Index()
+        {
+            var employees = new Employee().GetEmployees();
+            return View(employees);
+        }
+    }
+}
---
+++ MyProject/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿
+namespace MyProject.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Designation { get; set; }
+
+        public List<Employee> GetEmployees()
+        {
+            return new List<Employee>
+            {
+                new Employee {Id=1, Name="Anand", Designation="Sales Executive"},
+                new Employee {Id=2, Name="Praveen", Designation="Marketing Executive"},
+                new Employee {Id=3, Name="Kannan", Designation="Manager"},
+                new Employee {Id=4, Name="Jameel", Designation="System Admin"},
+            };
+        }
+    }
+}
